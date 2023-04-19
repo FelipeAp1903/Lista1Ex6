f7515f87b8933a88223d684ea5836ea02e7919a6
FelipeAp1903 <131301360+FelipeAp1903@users.noreply.github.com>
2023-04-19 16:58:51 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double n1;
+            Double n2;
+            Double soma;
+            Double media;
+            Double expoente;
+            Double Resultado;
+            Console.WriteLine("coloque o primeiro valor:");
+            n1 = Double.Parse(Console.ReadLine());
+            Console.WriteLine("coloque o segundo valor:");
+            n2 = Double.Parse(Console.ReadLine());
+            expoente = 1.0 / 2;
+            soma = n1 * n2;
+            media = Math.Pow(soma, expoente);
+            Resultado = media;
+            Console.WriteLine("A media Geometrica é:");
+            Console.WriteLine(media);
+        }
+    }
+}
